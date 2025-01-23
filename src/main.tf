285ebc964aed8873b984adaecb71b0efbd7c3440
kimcw2 <15kimcw@gmail.com>
2025-01-23 15:00:28 -0500

    ignoring local state files

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,4 @@
-module "tf-state-sbox" {
-    source = "./modules/tf_state"
+module "tf-state" {
+    source = "./modules/tf-state"
     bucket_name = "${var.state_bucket_name}-${var.env_abbrev}"
 }
