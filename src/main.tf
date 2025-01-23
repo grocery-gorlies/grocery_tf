6179b6567ef4b7483ca7f0a7be128ff9c7149230
kimcw2 <15kimcw@gmail.com>
2025-01-23 14:54:37 -0500

    comment out about to delete everything!

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,4 @@
-#module "tf-state-sbox" {
-#    source = "./modules/tf_state"
-#    bucket_name = "${var.state_bucket_name}-${var.env_abbrev}"
-#}
+module "tf-state-sbox" {
+    source = "./modules/tf_state"
+    bucket_name = "${var.state_bucket_name}-${var.env_abbrev}"
+}
