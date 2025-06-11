49cd87945a59bf51d21207f10bf3af460a5bb34a
kimcw2 <15kimcw@gmail.com>
2025-06-11 14:26:20 -0400

    not list

--- src/modules/iam/main.tf
+++ src/modules/iam/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "this" {
   name               = var.role_name
-  assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
   path               = "/${var.project_name}-${var.env_abbrev}/"
 
   tags = merge({
